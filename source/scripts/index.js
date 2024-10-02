1d94d491bdbf4d0ad800ac440405b82c1bebf604
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2024-10-02 09:13:30 +0200

    Вносит правки CSS для шапки и основного меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,33 +1,24 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const navMain = document.querySelector('.main-navigation');
-//   const navToggle = document.querySelector('.main-header__toggle');
-//   const burgerIcon = document.querySelector('.main-header__icon--burger');
-//   const closeIcon = document.querySelector('.main-header__icon--close');
 
-//   // Убираем класс no-js, если JavaScript работает
-//   navMain.classList.remove('main-navigation--nojs');
+const navMain = document.querySelector('.main-navigation');
+const navToggle = document.querySelector('.main-header__toggle');
+const burgerIcon = document.querySelector('.main-header__icon--burger');
+const closeIcon = document.querySelector('.main-header__icon--close');
 
-//   // Добавляем обработчик клика для кнопки
-//   navToggle.addEventListener('click', () => {
-//     const isMenuOpen = navMain.classList.contains('main-navigation--open');
+// Убираем класс no-js, если JavaScript работает
+navMain.classList.remove('main-navigation--nojs');
 
-//     // Переключаем классы меню
-//     if (isMenuOpen) {
-//       navMain.classList.remove('main-navigation--open');
-//       navMain.classList.add('main-navigation--close');
-//       navToggle.setAttribute('aria-expanded', 'false');
-
-//       // Меняем иконки
-//       closeIcon.style.display = 'none';
-//       burgerIcon.style.display = 'block';
-//     } else {
-//       navMain.classList.remove('main-navigation--close');
-//       navMain.classList.add('main-navigation--open');
-//       navToggle.setAttribute('aria-expanded', 'true');
-
-//       // Меняем иконки
-//       closeIcon.style.display = 'block';
-//       burgerIcon.style.display = 'none';
-//     }
-//   });
-// });
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--open');
+    navToggle.setAttribute('aria-expanded', 'false');
+    closeIcon.style.display = 'block';
+    burgerIcon.style.display = 'none';
+  } else {
+    navMain.classList.add('main-navigation--close');
+    navMain.classList.remove('main-navigation--open');
+    navToggle.setAttribute('aria-expanded', 'true');
+    closeIcon.style.display = 'none';
+    burgerIcon.style.display = 'block';
+  }
+});
