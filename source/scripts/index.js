aa584ea7c240e2311fd339bbfe0ffea1f3b81c89
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2024-10-06 01:22:36 +0200

    Добавляут webp, preload  и исправляет ошибки по переполнению

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,24 +1,24 @@
 
-const navMain = document.querySelector('.main-navigation');
-const navToggle = document.querySelector('.main-header__toggle');
-const burgerIcon = document.querySelector('.main-header__icon--burger');
-const closeIcon = document.querySelector('.main-header__icon--close');
+// const navMain = document.querySelector('.main-navigation');
+// const navToggle = document.querySelector('.main-header__toggle');
+// const burgerIcon = document.querySelector('.main-header__icon--burger');
+// const closeIcon = document.querySelector('.main-header__icon--close');
 
-// Убираем класс no-js, если JavaScript работает
-navMain.classList.remove('main-navigation--nojs');
+// // Убираем класс no-js, если JavaScript работает
+// navMain.classList.remove('main-navigation--nojs');
 
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-navigation--closed')) {
-    navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--open');
-    navToggle.setAttribute('aria-expanded', 'false');
-    closeIcon.style.display = 'block';
-    burgerIcon.style.display = 'none';
-  } else {
-    navMain.classList.add('main-navigation--close');
-    navMain.classList.remove('main-navigation--open');
-    navToggle.setAttribute('aria-expanded', 'true');
-    closeIcon.style.display = 'none';
-    burgerIcon.style.display = 'block';
-  }
-});
+// navToggle.addEventListener('click', () => {
+//   if (navMain.classList.contains('main-navigation--closed')) {
+//     navMain.classList.remove('main-navigation--closed');
+//     navMain.classList.add('main-navigation--open');
+//     navToggle.setAttribute('aria-expanded', 'false');
+//     closeIcon.style.display = 'block';
+//     burgerIcon.style.display = 'none';
+//   } else {
+//     navMain.classList.add('main-navigation--close');
+//     navMain.classList.remove('main-navigation--open');
+//     navToggle.setAttribute('aria-expanded', 'true');
+//     closeIcon.style.display = 'none';
+//     burgerIcon.style.display = 'block';
+//   }
+// });
